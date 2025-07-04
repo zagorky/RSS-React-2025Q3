58cc9994ab877f24b6dbc154ad546404bbe47db7
zagorky <zagorkiynahyi@gmail.com>
2025-07-04 16:43:50 +0300

    feat: add error section

--- rs-react-app/src/pages/main/components/error-section/error-section.tsx
+++ rs-react-app/src/pages/main/components/error-section/error-section.tsx
@@ -1,7 +1,17 @@
 import { Component } from 'react';
 
+import { Button } from '../../../../common-ui/button/button';
+
 export class ErrorSection extends Component {
   render() {
-    return <></>;
+    return (
+      <Button
+        onClick={() => {
+          throw new Error('~~Test error button clicked!~~');
+        }}
+      >
+        Throw Error
+      </Button>
+    );
   }
 }
