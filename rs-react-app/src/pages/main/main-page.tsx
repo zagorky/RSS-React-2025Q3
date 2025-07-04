98ed3b1ce0dd5ff95b8620ebfa5fc47b15ed0169
zagorky <zagorkiynahyi@gmail.com>
2025-07-04 18:07:44 +0300

    feat: update main page

--- rs-react-app/src/pages/main/main-page.tsx
+++ rs-react-app/src/pages/main/main-page.tsx
@@ -2,17 +2,34 @@ import { Component } from 'react';
 
 import { ErrorBoundary } from '../../components/error-boundary';
 import { ErrorFallback } from '../../components/error-fallback';
+import { retrieveQueryFormLS } from '../../utils/utilities';
+import { ErrorSection } from './components/error-section/error-section';
 import { ResultsSection } from './components/results-section/results-section';
 import { SearchForm } from './components/search-form/search-form';
 
-export class MainPage extends Component {
+interface State {
+  searchQuery: string;
+}
+
+export class MainPage extends Component<unknown, State> {
+  constructor(props: unknown) {
+    super(props);
+    const savedQuery = retrieveQueryFormLS();
+    this.state = { searchQuery: savedQuery };
+  }
+
+  handleSearch = (query: string) => {
+    this.setState({ searchQuery: query });
+  };
+
   render() {
     return (
       <main>
-        <SearchForm />
+        <SearchForm onSubmit={(query) => this.handleSearch(query)} />
         <ErrorBoundary fallback={<ErrorFallback />}>
-          <ResultsSection />
+          <ResultsSection searchQuery={this.state.searchQuery} />
         </ErrorBoundary>
+        <ErrorSection />
       </main>
     );
   }
