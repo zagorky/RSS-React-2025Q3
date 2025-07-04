09b40a45e9529fe7d1b30bfbff7bb4eff2a2c2c7
zagorky <zagorkiynahyi@gmail.com>
2025-07-04 18:06:19 +0300

    refactor: fix app config and api handler

--- rs-react-app/src/api/api.ts
+++ rs-react-app/src/api/api.ts
@@ -1,9 +1,9 @@
-import { apiEndpoints, apiUrl, LIMIT } from '../config/app-config';
+import { apiEndpoints, apiUrl } from '../config/app-config';
 
 const getUrl = (query?: string) => {
   return query
-    ? `${apiUrl}/${apiEndpoints.anime}?page=1?limit=${LIMIT}&offset=${LIMIT}?q=${query.trim()}`
-    : `${apiUrl}/${apiEndpoints.anime}?limit=${LIMIT}`;
+    ? `${apiUrl}/${apiEndpoints.anime}?q=${query.trim()}`
+    : `${apiUrl}/${apiEndpoints.anime}`;
 };
 
 export const fetchRequest = async (query?: string) => {
--- rs-react-app/src/config/app-config.ts
+++ rs-react-app/src/config/app-config.ts
@@ -1,7 +1,7 @@
 export const apiUrl = 'https://api.jikan.moe/v4';
 
 export const apiEndpoints = {
-  anime: '/anime',
+  anime: 'anime',
 };
 
 export const LIMIT = 10;
