4940d50b9aebe96b565cd9709f4ec3a77d31e21b
zagorky <zagorkiynahyi@gmail.com>
2025-07-04 18:07:29 +0300

    feat: add result section component

--- rs-react-app/src/pages/main/components/results-section/results-section.tsx
+++ rs-react-app/src/pages/main/components/results-section/results-section.tsx
@@ -1,7 +1,82 @@
 import { Component } from 'react';
 
-export class ResultsSection extends Component {
+import type { DataItem } from '../../../../types/types';
+
+import { fetchRequest } from '../../../../api/api';
+import { Loader } from '../../../../common-ui/loader/loader';
+import { ErrorFallback } from '../../../../components/error-fallback';
+import { normalizeError } from '../../../../utils/utilities';
+import { EmptyList } from '../empty-list/empty-list';
+import { ResultItem } from './result-item';
+
+type Props = {
+  searchQuery: string;
+};
+
+type State = {
+  results: DataItem[];
+  loading: boolean;
+  error: string | null;
+};
+
+export class ResultsSection extends Component<Props, State> {
+  state: State = {
+    results: [],
+    loading: false,
+    error: null,
+  };
+
+  async fetchData(query: string) {
+    this.setState({ loading: true, error: null });
+
+    try {
+      const data = await fetchRequest(query);
+      this.setState({
+        results: data.data ?? [],
+        loading: false,
+        error: null,
+      });
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: normalizeError(error),
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData(this.props.searchQuery);
+  }
+
+  componentDidUpdate(previousProps: Readonly<Props>) {
+    if (previousProps.searchQuery !== this.props.searchQuery) {
+      this.fetchData(this.props.searchQuery);
+    }
+  }
+
   render() {
-    return <></>;
+    const { loading, error, results } = this.state;
+    if (loading) {
+      return <Loader />;
+    }
+    if (error) {
+      return <ErrorFallback />;
+    }
+    if (!results) {
+      return <EmptyList />;
+    }
+    if (results.length === 0) {
+      return <EmptyList />;
+    }
+
+    return (
+      <section>
+        <ul>
+          {results.map((result) => (
+            <ResultItem key={result.title_english} data={result} />
+          ))}
+        </ul>
+      </section>
+    );
   }
 }
