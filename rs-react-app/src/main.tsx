390c8f109bd88ada63e04accd08f69a6f8bcdfd6
zagorky <zagorkiynahyi@gmail.com>
2025-07-04 12:20:57 +0300

    chore: configure project

---
+++ rs-react-app/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+const root = document.getElementById('root');
+
+if (root === null || root === undefined) {
+  throw new Error('Root must be provided');
+}
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
