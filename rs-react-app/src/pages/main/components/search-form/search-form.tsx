708fc49e091650caaaf789eb1e626f17861fec4e
zagorky <zagorkiynahyi@gmail.com>
2025-07-04 16:44:14 +0300

    feat: add a search form

--- rs-react-app/src/pages/main/components/search-form/search-form.tsx
+++ rs-react-app/src/pages/main/components/search-form/search-form.tsx
@@ -1,18 +1,48 @@
-import { Component, type FormEvent } from 'react';
+import { type ChangeEvent, Component, type FormEvent } from 'react';
 
-export class SearchForm extends Component {
+import { Button } from '../../../../common-ui/button/button';
+import { retrieveQueryFormLS, setQueryToLS } from '../../../../utils/utilities';
+
+type Props = {
+  onSubmit: (value: string) => void;
+};
+
+type State = {
+  value: string;
+};
+
+export class SearchForm extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    const retrievedQuery = retrieveQueryFormLS();
+    this.state = { value: retrievedQuery };
+  }
+
+  handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ value: event.target.value });
+  };
+
   handleSubmit(event: FormEvent) {
     event.preventDefault();
+    this.props.onSubmit(this.state.value);
+    setQueryToLS(this.state.value);
   }
 
   render() {
     return (
-      <form onSubmit={(event) => this.handleSubmit(event)}>
+      <form onSubmit={this.handleSubmit}>
         <label htmlFor="search-input">
-          Search
-          <input id="search-input" type="text" placeholder="Search" />
+          <span>Search</span>
+          <input
+            className="inpt"
+            value={this.state.value}
+            onChange={this.handleChange}
+            id="search-input"
+            type="text"
+            placeholder="Search"
+          />
         </label>
-        <button type="submit">Search</button>
+        <Button type="submit">Search</Button>
       </form>
     );
   }
