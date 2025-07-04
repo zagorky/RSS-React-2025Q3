3c1ff5464c74dc64c7a3483bc96b119903e8934c
zagorky <zagorkiynahyi@gmail.com>
2025-07-04 19:11:47 +0300

    refactor: update button component

--- rs-react-app/src/common-ui/button/button.tsx
+++ rs-react-app/src/common-ui/button/button.tsx
@@ -2,7 +2,7 @@ import { Component, type MouseEvent, type ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
-  className?: string;
+  classNames?: string;
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
   type?: 'button' | 'submit' | 'reset';
   disabled?: boolean;
@@ -15,8 +15,8 @@ export class Button extends Component<Props> {
   };
 
   render() {
-    const { className = '', onClick, type, disabled, children } = this.props;
-    const buttonClasses = `${className} btn`.trim();
+    const { classNames = '', onClick, type, disabled, children } = this.props;
+    const buttonClasses = `${classNames} btn`.trim();
     return (
       <button
         onClick={onClick}
