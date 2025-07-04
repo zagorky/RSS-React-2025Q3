20fb887a3a5fd133579c31e516f0876ef980adbf
zagorky <zagorkiynahyi@gmail.com>
2025-07-04 19:28:53 +0300

    refactor: update api

--- rs-react-app/src/api/api.ts
+++ rs-react-app/src/api/api.ts
@@ -8,7 +8,11 @@ const getUrl = (query?: string) => {
 
 export const fetchRequest = async (query?: string) => {
   const url = getUrl(query);
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
   if (!response.ok) {
     throw new Error(`Fetch error: ${response.statusText}`);
