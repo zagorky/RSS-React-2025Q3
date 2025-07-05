3a3e8b3d4c537de73bc38ae09aa3e2e65f7531f3
zagorky <zagorkiynahyi@gmail.com>
2025-07-05 16:49:36 +0300

    style: divide styles to the separate files and improve some style

--- rs-react-app/src/pages/main/components/results-section/result-item.tsx
+++ rs-react-app/src/pages/main/components/results-section/result-item.tsx
@@ -9,13 +9,21 @@ type Props = {
 export class ResultItem extends Component<Props, unknown> {
   title = this.props.data.title;
   synopsis = this.props.data.synopsis;
+  imgUrl = this.props.data.images.webp.image_url;
 
   render() {
     return (
       <li className="contents">
         <div className="result-item-wrapper">
           <p className="result-item-title">{this.title}</p>
-          <p className="result-item-decs">{this.synopsis}</p>
+          <div className="overflow-hidden rounded-md">
+            <img
+              className="result-item-img"
+              src={this.imgUrl}
+              alt={this.title}
+            />
+          </div>
+          <p className="result-item-desc">{this.synopsis}</p>
         </div>
       </li>
     );
