b267961abe6df8ab9fa8cd4f032270f10e7876ba
zagorky <zagorkiynahyi@gmail.com>
2025-07-07 20:31:15 +0300

    refactor: remove unnecessary fields from types

--- rs-react-app/src/types/types.ts
+++ rs-react-app/src/types/types.ts
@@ -7,38 +7,11 @@ export type DataItem = {
   mal_id: 0;
   url: string;
   images: { jpg: ImageType; webp: ImageType };
-  trailer: TrailerType;
-  approved: boolean;
-  titles: TitleType[];
   title: string;
-  title_english: string;
-  title_japanese: string;
-  title_synonyms: string[];
   type: Type;
-  source: string;
-  episodes: 0;
-  status: string;
-  airing: true;
-  duration: string;
-  rating: Rating;
-  score: number;
-  scored_by: number;
-  rank: number;
-  popularity: number;
   synopsis: string;
-  background: string;
-  season: string;
-  year: number;
   genres: GenresType[];
 };
-
-type Rating =
-  | 'G - All Ages'
-  | 'PG - Children'
-  | 'PG-13 - Teens 13 or older'
-  | 'R - 17+ (violence & profanity)'
-  | 'R+ - Mild Nudity'
-  | 'Rx - Hentai';
 
 type Type =
   | 'tv'
@@ -55,17 +28,6 @@ type ImageType = {
   image_url: string;
   small_image_url: string;
   large_image_url: string;
-};
-
-type TrailerType = {
-  youtube_id: string;
-  url: string;
-  embed_url: string;
-};
-
-type TitleType = {
-  type: string;
-  title: string;
 };
 
 type GenresType = {
