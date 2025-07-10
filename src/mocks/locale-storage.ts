6adf1361da5c5c48a2c528c89cf54e3a8af83479
zagorky <zagorkiynahyi@gmail.com>
2025-07-10 16:00:21 +0300

    feat: add search input tests

---
+++ src/mocks/locale-storage.ts
@@ -0,0 +1,2 @@
+export const getItemSpy = vi.spyOn(Storage.prototype, 'getItem');
+export const setItemSpy = vi.spyOn(Storage.prototype, 'setItem');
--- src/tests/search-component.test.tsx
+++ src/tests/search-component.test.tsx
@@ -1,7 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { LS_KEY } from '~config/app-config';
 import { SearchForm } from '~pages/main/components/search-form/search-form';
+import { expect } from 'vitest';
+
+import { getItemSpy, setItemSpy } from '~/mocks/locale-storage';
+
+const input = () => screen.getByTestId('search-form-input') as HTMLInputElement;
+const button = () => screen.getByTestId('search-form-submit-button');
 
 describe('Search Component', () => {
+  afterEach(() => {
+    getItemSpy.mockClear();
+    setItemSpy.mockClear();
+    localStorage.clear();
+  });
+
   test('should render search input and search button', () => {
     render(<SearchForm onSubmit={() => {}} />);
 
@@ -10,10 +23,52 @@ describe('Search Component', () => {
     expect(screen.getByTestId('search-form-submit-button')).toBeInTheDocument();
   });
 
-  test('should displays previously saved search term from localStorage', () => {});
-  test('should shows empty input when no saved term exists', () => {});
-  test('should updates input value when user types', () => {});
-  test('should saves search term to localStorage when search button is clicked', () => {});
-  test('should retrieves saved search term', () => {});
-  test('should overwrites existing localStorage value when new search is performed', () => {});
+  test('should displays previously saved search term from localStorage', () => {
+    getItemSpy.mockReturnValue('test1');
+    render(<SearchForm onSubmit={() => {}} />);
+
+    expect(getItemSpy).toHaveBeenCalledWith(LS_KEY);
+    expect(input().value).toBe('test1');
+  });
+
+  test('should shows empty input when no saved term exists', () => {
+    getItemSpy.mockReturnValue(null);
+    render(<SearchForm onSubmit={() => {}} />);
+
+    expect(getItemSpy).toHaveBeenCalledWith(LS_KEY);
+    expect(input().value).toBe('');
+  });
+
+  test('should saves search term to localStorage when search button is clicked', () => {
+    const onSubmit = vi.fn();
+    render(<SearchForm onSubmit={onSubmit} />);
+
+    fireEvent.change(input(), { target: { value: 'naruto' } });
+    fireEvent.click(button());
+
+    expect(setItemSpy).toHaveBeenCalledWith(LS_KEY, 'naruto');
+    expect(onSubmit).toHaveBeenCalledWith('naruto');
+  });
+
+  test('should retrieves saved search term', () => {
+    getItemSpy.mockReturnValue('test1');
+    render(<SearchForm onSubmit={() => {}} />);
+
+    expect(getItemSpy).toHaveBeenCalledWith(LS_KEY);
+    expect(input().value).toBe('test1');
+  });
+
+  test('should overwrites existing localStorage value when new search is performed', () => {
+    getItemSpy.mockReturnValue('oldQuery');
+    const onSubmit = vi.fn();
+    render(<SearchForm onSubmit={onSubmit} />);
+
+    expect(input().value).toBe('oldQuery');
+
+    fireEvent.change(input(), { target: { value: 'newQuery' } });
+    fireEvent.click(button());
+
+    expect(setItemSpy).toHaveBeenCalledWith(LS_KEY, 'newQuery');
+    expect(onSubmit).toHaveBeenCalledWith('newQuery');
+  });
 });
