23d0d7f8f5f4801096a681a791819a1fdc5e0770
zagorky <zagorkiynahyi@gmail.com>
2025-07-20 20:07:19 +0300

    refactor: refactor loader component

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,17 +1,13 @@
 import type { ReactNode } from 'react';
 
-import { Component } from 'react';
-
 type LayoutProps = {
   children: ReactNode;
 };
 
-export class Layout extends Component<LayoutProps> {
-  render() {
-    return (
-      <div className="main-wrapper">
-        <main className="main">{this.props.children}</main>
-      </div>
-    );
-  }
-}
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="main-wrapper">
+      <main className="main">{children}</main>
+    </div>
+  );
+};
--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,15 +1,12 @@
 import { withDataTestId } from '~utils/utilities';
-import { Component } from 'react';
 
-export class Loader extends Component {
-  render() {
-    return (
-      <div className="loader-overlay">
-        <div {...withDataTestId('loader')} className="loader-container">
-          <div className="loader-spinner"></div>
-          <span className="loader-text">Loading...</span>
-        </div>
+export const Loader = () => {
+  return (
+    <div className="loader-overlay">
+      <div {...withDataTestId('loader')} className="loader-container">
+        <div className="loader-spinner"></div>
+        <span className="loader-text">Loading...</span>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
