3a545b64b7b80a4632b12b4dd81b8607d036f30f
zagorky <zagorkiynahyi@gmail.com>
2025-07-20 19:42:38 +0300

    refactor: refactor search form component

--- src/pages/main/components/search-form/search-form.tsx
+++ src/pages/main/components/search-form/search-form.tsx
@@ -1,43 +1,40 @@
 import { Button } from '~components/button/button';
 import { isString } from '~types/type-guards';
 import { withDataTestId } from '~utils/utilities';
-import { Component, type FormEvent } from 'react';
+import { type FormEvent } from 'react';
 
 type SearchFormProps = {
   onSubmit: (value: string) => void;
   searchQuery: string;
 };
 
-export class SearchForm extends Component<SearchFormProps> {
-  handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+export const SearchForm = ({ onSubmit, searchQuery }: SearchFormProps) => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const inputFormData = new FormData(event.currentTarget);
     const formValue = inputFormData.get('search-input');
     if (isString(formValue)) {
-      this.props.onSubmit(formValue);
+      onSubmit(formValue);
     }
   };
-
-  render() {
-    return (
-      <form
-        {...withDataTestId('search-form')}
-        className="search-form"
-        onSubmit={this.handleSubmit}
-      >
-        <label>
-          <span className="sr-only">Search</span>
-          <input
-            {...withDataTestId('search-form-input')}
-            name="search-input"
-            defaultValue={this.props.searchQuery}
-            className="inpt"
-            type="text"
-            placeholder="Search"
-          />
-        </label>
-        <Button type="submit">Search</Button>
-      </form>
-    );
-  }
-}
+  return (
+    <form
+      {...withDataTestId('search-form')}
+      className="search-form"
+      onSubmit={handleSubmit}
+    >
+      <label>
+        <span className="sr-only">Search</span>
+        <input
+          {...withDataTestId('search-form-input')}
+          name="search-input"
+          defaultValue={searchQuery}
+          className="inpt"
+          type="text"
+          placeholder="Search"
+        />
+      </label>
+      <Button type="submit">Search</Button>
+    </form>
+  );
+};
