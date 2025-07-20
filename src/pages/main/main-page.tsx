b5dee1918b548553dee2bab51713665595ab2985
zagorky <zagorkiynahyi@gmail.com>
2025-07-20 19:37:58 +0300

    refactor: refactor main page component

--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -1,34 +1,22 @@
 import { retrieveQueryFormLS, setQueryToLS } from '~utils/utilities';
-import { Component } from 'react';
+import { useState } from 'react';
 
 import { ErrorButton } from './components/error-section/error-button';
 import { ResultsSection } from './components/results-section/results-section';
 import { SearchForm } from './components/search-form/search-form';
 
-type MainPageState = {
-  searchQuery: string;
-};
+export const MainPage = () => {
+  const [searchQuery, setSearchQuery] = useState(retrieveQueryFormLS());
 
-export class MainPage extends Component<unknown, MainPageState> {
-  state = {
-    searchQuery: retrieveQueryFormLS(),
-  };
-
-  handleSearch = (query: string) => {
-    this.setState({ searchQuery: query });
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
     setQueryToLS(query);
   };
-
-  render() {
-    return (
-      <>
-        <SearchForm
-          searchQuery={this.state.searchQuery}
-          onSubmit={this.handleSearch}
-        />
-        <ResultsSection searchQuery={this.state.searchQuery} />
-        <ErrorButton />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <SearchForm searchQuery={searchQuery} onSubmit={handleSearch} />
+      <ResultsSection searchQuery={searchQuery} />
+      <ErrorButton />
+    </>
+  );
+};
