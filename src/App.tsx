bc85b2ddd93945b0ddb07e5c755546b92e971914
zagorky <zagorkiynahyi@gmail.com>
2025-07-20 20:14:48 +0300

    refactor: refactor app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Layout } from '~components/layout/layout';
 import { MainPage } from '~pages/main/main-page';
 
-function App() {
+const App = () => {
   return (
     <Layout>
       <MainPage />
     </Layout>
   );
-}
+};
 
 export default App;
