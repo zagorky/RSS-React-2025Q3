ba41fd29cb3b9b8b0c2cf50c53e12d685e3cac57
zagorky <zagorkiynahyi@gmail.com>
2025-07-20 20:06:01 +0300

    refactor: refactor empty list component

--- src/pages/main/components/empty-list/empty-list.tsx
+++ src/pages/main/components/empty-list/empty-list.tsx
@@ -1,13 +1,10 @@
 import { withDataTestId } from '~utils/utilities';
-import { Component } from 'react';
 
-export class EmptyList extends Component {
-  render() {
-    return (
-      <div {...withDataTestId('empty-list')} className="basic-content-wrapper">
-        <h2 className="h2">No matching</h2>
-        <p>Try another search query</p>
-      </div>
-    );
-  }
-}
+export const EmptyList = () => {
+  return (
+    <div {...withDataTestId('empty-list')} className="basic-content-wrapper">
+      <h2 className="h2">No matching</h2>
+      <p>Try another search query</p>
+    </div>
+  );
+};
