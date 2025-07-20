9b6eda5c2fe160c9887769340a73694007dc0b7d
zagorky <zagorkiynahyi@gmail.com>
2025-07-20 20:01:41 +0300

    refactor: refactor error button component

--- src/pages/main/components/error-section/error-button.tsx
+++ src/pages/main/components/error-section/error-button.tsx
@@ -1,30 +1,46 @@
 import { Button } from '~components/button/button';
-import { Component } from 'react';
+import { useState } from 'react';
 
-type ErrorButtonState = {
-  shouldThrow: boolean;
-};
+// export class ErrorButton extends Component<unknown, ErrorButtonState> {
+//   state = {
+//     shouldThrow: false,
+//   };
+//
+//   triggerError = () => {
+//     this.setState({ shouldThrow: true });
+//   };
+//
+//   render() {
+//     if (this.state.shouldThrow) {
+//       throw new Error('~~Test error button clicked!~~');
+//     }
+//
+//     return (
+//       <section className="error-section">
+//         <Button dataTestId={'throw-error-button'} onClick={this.triggerError}>
+//           Throw Error
+//         </Button>
+//       </section>
+//     );
+//   }
+// }
 
-export class ErrorButton extends Component<unknown, ErrorButtonState> {
-  state = {
-    shouldThrow: false,
+export const ErrorButton = () => {
+  const [shouldThrow, setShouldThrow] = useState(false);
+
+  const triggerError = () => {
+    setShouldThrow(true);
   };
 
-  triggerError = () => {
-    this.setState({ shouldThrow: true });
-  };
-
-  render() {
-    if (this.state.shouldThrow) {
-      throw new Error('~~Test error button clicked!~~');
-    }
-
-    return (
-      <section className="error-section">
-        <Button dataTestId={'throw-error-button'} onClick={this.triggerError}>
-          Throw Error
-        </Button>
-      </section>
-    );
+  if (shouldThrow) {
+    throw new Error('~~Test error button clicked!~~');
   }
-}
+
+  return (
+    <section className="error-section">
+      <Button dataTestId={'throw-error-button'} onClick={triggerError}>
+        Throw Error
+      </Button>
+    </section>
+  );
+};
