fe23a77e61ad2a98c46be500e3adcad3c5b1f7f0
zagorky <zagorkiynahyi@gmail.com>
2025-07-20 20:09:00 +0300

    refactor: refactor error fallback component

--- src/components/error-fallback/error-fallback.tsx
+++ src/components/error-fallback/error-fallback.tsx
@@ -1,32 +1,25 @@
 import { Button } from '~components/button/button';
 import { withDataTestId } from '~utils/utilities';
-import { Component } from 'react';
 
 type ErrorFallbackProps = {
   error: Error | string;
 };
 
-export class ErrorFallback extends Component<ErrorFallbackProps> {
-  render() {
-    return (
-      <section
-        {...withDataTestId('error-fallback')}
-        id="error-fallback"
-        className="basic-content-wrapper"
+export const ErrorFallback = ({ error }: ErrorFallbackProps) => {
+  return (
+    <section
+      {...withDataTestId('error-fallback')}
+      id="error-fallback"
+      className="basic-content-wrapper"
+    >
+      <h2 className="h2 text-[var(--color-error)]">Try Again</h2>
+      <p>{error instanceof Error ? error.message : error}</p>
+      <Button
+        dataTestId={'error-fallback-button'}
+        onClick={() => window.location.reload()}
       >
-        <h2 className="h2 text-[var(--color-error)]">Try Again</h2>
-        <p>
-          {this.props.error instanceof Error
-            ? this.props.error.message
-            : this.props.error}
-        </p>
-        <Button
-          dataTestId={'error-fallback-button'}
-          onClick={() => window.location.reload()}
-        >
-          Refresh
-        </Button>
-      </section>
-    );
-  }
-}
+        Refresh
+      </Button>
+    </section>
+  );
+};
