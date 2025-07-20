66293902ea5d75963af0752ff637e1e27dfe09cb
zagorky <zagorkiynahyi@gmail.com>
2025-07-20 20:02:04 +0300

    refactor: refactor error button component

--- src/pages/main/components/error-section/error-button.tsx
+++ src/pages/main/components/error-section/error-button.tsx
@@ -1,30 +1,6 @@
 import { Button } from '~components/button/button';
 import { useState } from 'react';
 
-// export class ErrorButton extends Component<unknown, ErrorButtonState> {
-//   state = {
-//     shouldThrow: false,
-//   };
-//
-//   triggerError = () => {
-//     this.setState({ shouldThrow: true });
-//   };
-//
-//   render() {
-//     if (this.state.shouldThrow) {
-//       throw new Error('~~Test error button clicked!~~');
-//     }
-//
-//     return (
-//       <section className="error-section">
-//         <Button dataTestId={'throw-error-button'} onClick={this.triggerError}>
-//           Throw Error
-//         </Button>
-//       </section>
-//     );
-//   }
-// }
-
 export const ErrorButton = () => {
   const [shouldThrow, setShouldThrow] = useState(false);
 
