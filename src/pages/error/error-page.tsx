b079217717a67d8a4597ef7900043b95a1a8c1be
zagorky <zagorkiynahyi@gmail.com>
2025-07-22 16:35:45 +0300

    feat: add error page

--- src/pages/error/error-page.tsx
+++ src/pages/error/error-page.tsx
@@ -1,15 +1,28 @@
-import { useRouteError } from 'react-router';
+import { Button } from '~components/button/button';
+import { navigation } from '~config/navigation';
+import { withDataTestId } from '~utils/utilities';
+import { Link, useRouteError } from 'react-router';
 
 const ErrorPage = () => {
   const error = useRouteError();
+  const errorName = error instanceof Error ? error.name : '404: Page Not Found';
+  const errorMessage =
+    error instanceof Error ? error.message : 'Something went wrong';
 
   return (
-    <div className="m-auto">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error instanceof Error ? error.message : 'Not Found'}</i>
-      </p>
+    <div
+      {...withDataTestId('error-fallback')}
+      className="m-auto flex h-[calc(100vh-200px)] max-w-xl flex-col items-center justify-center gap-10"
+    >
+      <h1 className="text-6xl text-[var(--color-error)]">{errorName}</h1>
+      <h2>{errorMessage}</h2>
+      <div>
+        <Button classNames="m-4" dataTestId={'error-fallback-button'}>
+          <Link className="p-4" to={navigation.main}>
+            Back to main page
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 };
