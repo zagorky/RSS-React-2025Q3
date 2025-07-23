11f76c208d90f0afaa344092f709ae9992f586a2
zagorky <zagorkiynahyi@gmail.com>
2025-07-23 20:21:17 +0300

    refactor: add styles

--- src/pages/main/components/results-section/results-section.tsx
+++ src/pages/main/components/results-section/results-section.tsx
@@ -47,7 +47,7 @@ export const ResultsSection = ({
           className={cn(
             'grid w-full gap-6 transition-all duration-300 ease-in-out',
             outlet
-              ? 'grid-cols-1 sm:grid-cols-2'
+              ? 'grid-cols-1 md:grid-cols-2'
               : 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'
           )}
         >
