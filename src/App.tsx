bc46743c8bca43edc3e356990c0a504c8766eda7
zagorky <zagorkiynahyi@gmail.com>
2025-08-03 12:55:34 +0300

    chore: add tanstack eslint and add query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '~components/theme-switcher/theme-provider';
 import { router } from '~pages/app-router';
 import { RouterProvider } from 'react-router';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
