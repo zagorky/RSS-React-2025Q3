5a780ce2b4130477a70ce94d6d6f87bff693fb7f
zagorky <zagorkiynahyi@gmail.com>
2025-08-11 16:21:32 +0300

    chore: add nextjs config, refactor gitignore, tsconfig, package json, add root layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type {Metadata} from 'next';
+
+import {type ReactNode} from 'react';
+
+// import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'HUH App',
+  keywords:
+    'anime, search anime, Sousou no Frieren, Cowboy Bebop, anime database',
+  openGraph: {
+    title: 'Anime Search - Find Your Favorite Anime',
+    description:
+      'Explore anime titles, synopses, and genres with our fast and easy-to-use search tool.',
+    type: 'website',
+  },
+  description:
+    'Search for anime like Sousou no Frieren, Cowboy Bebop, and more. Discover details, genres, and synopses.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
