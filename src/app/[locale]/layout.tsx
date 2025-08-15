6f999e8b970beb0e953b486d0cb0f88a40c2e8ba
zagorky <zagorkiynahyi@gmail.com>
2025-08-15 15:15:48 +0300

    refactor: fix theme reset after change locale

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,11 +1,11 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 
-import {routing} from '~i18n/routing';
-import {Header} from '~ui/header/header';
-import {ThemeProvider} from '~ui/theme-switcher/theme-provider';
-import {hasLocale, NextIntlClientProvider} from 'next-intl';
-import {notFound} from 'next/navigation';
-import {type ReactNode} from 'react';
+import { routing } from '~i18n/routing';
+import { Header } from '~ui/header/header';
+import { ThemeProvider } from '~ui/theme-switcher/theme-provider';
+import { hasLocale, NextIntlClientProvider } from 'next-intl';
+import { notFound } from 'next/navigation';
+import { type ReactNode } from 'react';
 
 export const metadata: Metadata = {
   title: 'HUH App',
--- src/app/ui/theme-switcher/theme-provider.tsx
+++ src/app/ui/theme-switcher/theme-provider.tsx
@@ -1,25 +1,22 @@
 'use client';
-import { useThemeStore } from '~store/theme-store';
-import { type ThemeProviderProps } from '~types/theme';
-import { useLayoutEffect } from 'react';
+import {useThemeStore} from '~store/theme-store';
+import {type ThemeProviderProps} from '~types/theme';
+import {useLayoutEffect} from 'react';
 
-import { ThemeProviderContext } from './theme-provider-context';
+import {ThemeProviderContext} from './theme-provider-context';
 
 export const ThemeProvider = ({ children }: ThemeProviderProps) => {
   const { theme, actions } = useThemeStore();
 
   useLayoutEffect(() => {
     actions.updateSystemTheme();
+    actions.applyCurrentTheme();
 
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
     const handler = () => actions.updateSystemTheme();
     mediaQuery.addEventListener('change', handler);
 
     return () => mediaQuery.removeEventListener('change', handler);
-  }, [actions]);
-
-  useLayoutEffect(() => {
-    actions.applyCurrentTheme();
   }, [actions]);
 
   return (
