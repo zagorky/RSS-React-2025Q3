0f6cc25e6c222a9dc5f465d3ff5115e76696f836
zagorky <zagorkiynahyi@gmail.com>
2025-08-15 20:02:49 +0300

    refactor: add intl and fix styles

--- src/app/ui/modal/modal.tsx
+++ src/app/ui/modal/modal.tsx
@@ -4,8 +4,8 @@ import {DetailedPage} from '~ui/detail-page/detailed-page';
 
 const Modal = ({ data }: { data: DataItem }) => {
   return (
-    <div className="fixed top-20 right-0 z-50 m-10 overflow-hidden">
-      <section className="w-full max-w-[500px] justify-items-center overflow-y-auto">
+    <div className="fixed top-0 right-0 z-50 m-5 overflow-hidden">
+      <section className="max-h-screen w-full max-w-[500px] justify-items-center overflow-y-auto">
         <DetailedPage data={data} />
       </section>
     </div>
