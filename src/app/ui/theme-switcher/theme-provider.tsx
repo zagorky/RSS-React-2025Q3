a403c5f2f84de2f015c6371dbce4745f5157ecb2
zagorky <zagorkiynahyi@gmail.com>
2025-08-16 19:31:44 +0300

    refactor: refactor theme provider

--- src/app/ui/theme-switcher/theme-provider.tsx
+++ src/app/ui/theme-switcher/theme-provider.tsx
@@ -1,28 +1,27 @@
 'use client';
-import { useThemeStore } from '~store/theme-store';
+import { useTheme, useThemeActions } from '~store/theme-store';
 import { type ThemeProviderProps } from '~types/theme';
 import { useLayoutEffect } from 'react';
 
 import { ThemeProviderContext } from './theme-provider-context';
 
 export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  const { theme, actions } = useThemeStore();
+  const theme = useTheme();
+  const { updateSystemTheme, applyCurrentTheme, setTheme } = useThemeActions();
 
   useLayoutEffect(() => {
-    actions.updateSystemTheme();
-    actions.applyCurrentTheme();
+    updateSystemTheme();
+    applyCurrentTheme();
 
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    const handler = () => actions.updateSystemTheme();
+    const handler = () => updateSystemTheme();
     mediaQuery.addEventListener('change', handler);
 
     return () => mediaQuery.removeEventListener('change', handler);
-  }, [actions]);
+  }, [applyCurrentTheme, updateSystemTheme]);
 
   return (
-    <ThemeProviderContext.Provider
-      value={{ theme, setTheme: actions.setTheme }}
-    >
+    <ThemeProviderContext.Provider value={{ theme, setTheme: setTheme }}>
       {children}
     </ThemeProviderContext.Provider>
   );
