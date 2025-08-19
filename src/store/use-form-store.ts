b31bdc74dad20209360fb047ac0c3257770cb4cb
zagorky <zagorkiynahyi@gmail.com>
2025-08-19 13:00:58 +0300

    refactor:refactor formsStore

--- src/store/use-form-store.ts
+++ src/store/use-form-store.ts
@@ -1,6 +1,12 @@
-import { create } from 'zustand';
+import type {z} from 'zod';
 
-import type { FormType } from '~/tests/form-types';
+import {type formSchema} from '~types/form-types';
+import {create} from 'zustand';
+
+type FormType = z.infer<typeof formSchema> & {
+  id: string;
+  createAt: Date | string;
+};
 
 type FormStoreType = {
   forms: FormType[];
@@ -9,10 +15,12 @@ type FormStoreType = {
   };
 };
 
-export const useFormStore = () =>
-  create<FormStoreType>()((set) => ({
-    forms: [],
-    actions: {
-      addForm: (form: FormType) => set((state) => ({ forms: [...state.forms, form] })),
-    },
-  }));
+const useFormStore = create<FormStoreType>()((set) => ({
+  forms: [],
+  actions: {
+    addForm: (form: FormType) => set((state) => ({ forms: [...state.forms, form] })),
+  },
+}));
+
+export const useCollectedForms = () => useFormStore((state) => state.forms);
+export const useFormStoreActions = () => useFormStore((state) => state.actions);
