8868b401dc73415a0019747630647bd5b088b9fe
zagorky <zagorkiynahyi@gmail.com>
2025-08-19 22:27:57 +0300

    refactor: divide form field, refactor forms

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,11 +1,14 @@
 import { CollectedForms } from '~components/collected-forms';
 import { ControlledForm } from '~components/controlled-form';
+import { UncontrolledForm } from '~components/uncontrolled-form';
 
 const MainPage = () => {
   return (
     <>
-      {/*<UncontrolledForm />*/}
-      <ControlledForm />
+      <div className="flex">
+        <UncontrolledForm />
+        <ControlledForm />
+      </div>
       <CollectedForms />
     </>
   );
